4865e7942b1b4ed43f914b4929bab2abe0ee80d1
Rasmus Vind <noreply@vindit.dk>
2019-10-23 16:01:20 +0200

    Make parallel script run 6 scripts instead of 4.

--- src/run_parallel.sh
+++ src/run_parallel.sh
@@ -5,7 +5,9 @@ set -e
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
-parallel ::: "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-1-of-4" \
-             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-2-of-4" \
-             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-3-of-4" \
-             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-4-of-4"
+parallel ::: "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-1-of-6" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-2-of-6" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-3-of-6" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-4-of-6" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-5-of-6" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-6-of-6"
