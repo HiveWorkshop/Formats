e87ffc8b16a81ac8b1033e2861e640598dae65e4
Rasmus Vind <noreply@vindit.dk>
2019-10-23 15:30:17 +0200

    Make parallel script more readable.

--- src/run_parallel.sh
+++ src/run_parallel.sh
@@ -5,4 +5,7 @@ set -e
 
 DIR="$( dirname "${BASH_SOURCE[0]}" )"
 
-parallel ::: "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-1-of-4" "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-2-of-4" "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-3-of-4" "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-4-of-4"
+parallel ::: "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-1-of-4" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-2-of-4" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-3-of-4" \
+             "php $DIR/test.php --parse-dir $DIR/../samples/wtg_old --split-4-of-4"
